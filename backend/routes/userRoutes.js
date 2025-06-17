64e05cc8867c3e405d9a5e75ad9a91ac16078bd3
Shivam <shivamsisodiya2055@gmail.com>
2025-06-17 07:03:46 +0530

    Correct

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,10 +9,10 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.post('/send-verify-otp', userAuth, sendVerifyOtp);
-router.post('/verify-account', userAuth, verifyEmail);
+router.post('/send-verify-otp', userAuth, sendVerifyOtp) //user auth
+router.post('/verify-account', userAuth, verifyEmail);  //user auth
 
-router.get('/is-auth', userAuth, isAuthenticated);
+router.get('/is-auth', userAuth, isAuthenticated); //userAuth
 router.post("/send-reset-otp", sendResetOtp);
 
 router.post('/reset-password', resetPassword);
